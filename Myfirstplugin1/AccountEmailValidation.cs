d39f78f71f5e3b95588b93baca835d4d3743282c
user <user@VIVIANEMILY>
2026-05-07 16:58:43 +0100

    Add project files.

---
+++ Myfirstplugin1/AccountEmailValidation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+public class AccountEmailValidation : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+        if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+        {
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            if (entity.LogicalName != "account")
+                return;
+
+            string email = entity.GetAttributeValue<string>("emailaddress");
+
+            if (string.IsNullOrEmpty(email))
+            {
+                throw new InvalidPluginExecutionException("Email is required.");
+            }
+        }
+    }
+}
